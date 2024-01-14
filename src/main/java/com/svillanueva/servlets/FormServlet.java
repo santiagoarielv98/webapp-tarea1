5ca1feff7c43520903a4434ae2ee8b58df4ae9bd
Santiago Ariel <santiagoarielv98@gmail.com>
2024-01-14 14:12:03 -0300

    cambios

--- src/main/java/com/svillanueva/servlets/FormServlet.java
+++ src/main/java/com/svillanueva/servlets/FormServlet.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/registro")
 public class FormServlet extends HttpServlet {
@@ -20,43 +22,80 @@ public class FormServlet extends HttpServlet {
 		String pais = req.getParameter("pais");
 		String[] lenguajes = req.getParameterValues("lenguajes");
 		String[] roles = req.getParameterValues("roles");
+		String idioma = req.getParameter("idioma");
+		boolean habilitar = req.getParameter("habilitar") != null && req.getParameter("habilitar")
+				.equals("on");
+		String secreto = req.getParameter("secreto");
+
+		List<String> errores = new ArrayList<>();
+
+		if (username == null || username.isBlank()) {
+			errores.add("el username se requerido!");
+		}
+		if (password == null || password.isBlank()) {
+			errores.add("el password no puede ser vació!");
+		}
+		if (email == null || !email.contains("@")) {
+			errores.add("el email es requerido y debe tener un formato de correo.");
+		}
+		if (pais == null || pais.isBlank()) {
+			errores.add("el pais es requerido!");
+		}
+		if (lenguajes == null || lenguajes.length == 0) {
+			errores.add("debe seleccionar al menos un tema.");
+		}
+		if (roles == null || roles.length == 0) {
+			errores.add("debe seleccionar al menos un rol.");
+		}
+		if (idioma == null) {
+			errores.add("debe seleccionar un idioma");
+		}
+
+		PrintWriter out = resp.getWriter();
 
 
-		PrintWriter printWriter = resp.getWriter();
-
-		printWriter.println("<!doctype html>");
-		printWriter.println("<html lang=\"en\">");
-		printWriter.println("	<head>");
-		printWriter.println("		<meta charset=\"UTF-8\">");
-		printWriter.println("		<meta name=\"viewport\"\n" +
+		out.println("<!doctype html>");
+		out.println("<html lang=\"en\">");
+		out.println("	<head>");
+		out.println("		<meta charset=\"UTF-8\">");
+		out.println("		<meta name=\"viewport\"\n" +
 				"\t\tcontent=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">");
-		printWriter.println("		<meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">");
-		printWriter.println("		<title>Index Servlet</title>");
-		printWriter.println("	</head>");
-		printWriter.println("	<body>");
+		out.println("		<meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">");
+		out.println("		<title>Index Servlet</title>");
+		out.println("	</head>");
+		out.println("	<body>");
 
-		printWriter.println("		<h1>Resultado Form: </h1>");
+		out.println("		<h1>Resultado Form: </h1>");
 
-		printWriter.println("		<ul>");
-		printWriter.println("			<li>Username: " + username + "</li>");
-		printWriter.println("			<li>Password: " + password + "</li>");
-		printWriter.println("			<li>Email: " + email + "</li>");
-		printWriter.println("			<li>Pais: " + pais + "</li>");
-		printWriter.println("			<li><ul>");
-		for (String lenguaje : lenguajes) {
-			printWriter.println("				<li>" + lenguaje + "</li>");
+		out.println("		<ul>");
+		if (errores.isEmpty()) {
+
+			out.println("			<li>Username: " + username + "</li>");
+			out.println("			<li>Password: " + password + "</li>");
+			out.println("			<li>Email: " + email + "</li>");
+			out.println("			<li>Pais: " + pais + "</li>");
+			out.println("			<li><ul>");
+			for (String lenguaje : lenguajes) {
+				out.println("				<li>" + lenguaje + "</li>");
+			}
+			out.println("			</ul></li>");
+			out.println("			<li><ul>");
+			for (String rol : roles) {
+				out.println("				<li>" + rol + "</li>");
+			}
+			out.println("			</ul></li>");
+			out.println("		<li>Idioma:" + idioma + "</li>");
+			out.println("		<li>Habilitado: " + habilitar + "</li>");
+			out.println("		<li>Secreto: " + secreto + "</li>");
+		} else {
+			errores.forEach(error -> out.println("<li>" + error + "</li>"));
+			out.println("<p> <a href=\"/webapp-tarea1/form.html\"> volver </a> </p>");
 		}
-		printWriter.println("			</ul></li>");
-		printWriter.println("			<li><ul>");
-		for (String rol : roles) {
-			printWriter.println("				<li>" + rol + "</li>");
-		}
-		printWriter.println("			</ul></li>");
 
-		printWriter.println("		</ul>");
+		out.println("		</ul>");
 
-		printWriter.println("	</body>");
-		printWriter.println("</html>");
-		printWriter.close();
+		out.println("	</body>");
+		out.println("</html>");
+		out.close();
 	}
 }
